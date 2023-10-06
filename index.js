efcb367fdc8fa87b3dfb83a61c9c9c8c8749ed1e
Ian Jeremy Guani <ianguani@yahoo.ca>
2023-10-06 10:40:21 -0700

    basic ejs, node, express, express-sessions to start our project off

---
+++ index.js
@@ -0,0 +1,32 @@
+require('./utils');
+require('dotenv').config();
+
+const express = require('express');
+const session = require('express-session');
+
+const port = process.env.PORT || 3000;
+const node_session_secret = process.env.NODE_SESSION_SECRET;
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({extended: false}));
+
+app.use(session({ 
+    secret: node_session_secret,
+    //default is memory store 
+	saveUninitialized: false, 
+	resave: true
+}
+));
+
+app.get('/', (req, res) => {
+    res.render("index")
+})
+
+// app.use(express.static(__dirname + "/public"));
+
+app.listen(port, () => {
+	console.log("Node application listening on port "+port);
+}); 
