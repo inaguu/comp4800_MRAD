cf209c9c1d8f34345f548d22b13f6b5fcc4c1276
Winston <winston_nguyenn@hotmail.com>
2023-11-07 23:38:15 -0800

    resolved conflicts

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,6 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
     extend: {
-<<<<<<< HEAD
-      colors: {
-        'babyblue': '#BBE2DF',
-        'cyan': '#9BD4CD',
-      }
-=======
->>>>>>> main
     },
   },
   plugins: [],
