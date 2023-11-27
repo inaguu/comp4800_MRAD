8520df6d11f392218ecfaf1d602914f7d0f34a4f
Nav <navdeepnx@gmail.com>
2023-11-27 08:02:35 -0800

    reverted commit

--- database/admin.js
+++ database/admin.js
@@ -22,36 +22,11 @@ async function getStudents(postData) {
 }
 
 async function getOneStudent(postData) {
-	// let getOneStudentSQL = `
-    //     SELECT *
-    //     FROM users
-    //     JOIN user_type USING (user_type_id)
-    //     WHERE MRAD_id = :MRADid;
-	// `;
-
-	let getStudentChoicesSQL = `
-	SELECT u.*, ut.type,
-	cs1.site_name AS choice_1_site_name
-	cs2.site_name AS choice_2_site_name
-	cs3.site_name AS choice_3_site_name
-	cs4.site_name AS choice_4_site_name
-	cs5.site_name AS choice_5_site_name
-	FROM users u
-	JOIN
-	user_type ut ON u.user_type_id = ut.user_type_id
-	LEFT JOIN
-	student_choices sc ON sc.user_id = u.user_id
-	LEFT JOIN
-	clinical_sites cs1 ON cs1.clinical_sites_id = sc.choice_1
-	LEFT JOIN
-	clinical_sites cs2 ON cs2.clinical_sites_id = sc.choice_2
-	LEFT JOIN
-	clinical_sites cs3 ON cs3.clinical_sites_id = sc.choice_3
-	LEFT JOIN
-	clinical_sites cs4 ON cs4.clinical_sites_id = sc.choice_4
-	LEFT JOIN
-	clinical_sites cs5 ON cs5.clinical_sites_id = sc.choice_5
-	WHERE u.MRAD_id = :MRADid;
+	let getOneStudentSQL = `
+        SELECT *
+        FROM users
+        JOIN user_type USING (user_type_id)
+        WHERE MRAD_id = :MRADid;
 	`;
 
 	let params = {
@@ -59,7 +34,7 @@ async function getOneStudent(postData) {
 	};
 
 	try {
-		const results = await database.query(getStudentChoicesSQL, params);
+		const results = await database.query(getOneStudentSQL, params);
 
 		console.log("Successfully found students MRAD IDs");
 		console.log(results);
