e8f166dffe2832f166e83fcd69b3cc9c32d7ba55
krispyrendang <taswinder.s@gmail.com>
2023-11-28 06:39:56 -0800

    Updated SQL query to grab clinical placement selection by single student. Updated ejs to display results.

--- database/admin.js
+++ database/admin.js
@@ -46,46 +46,72 @@ async function getOneStudent(postData) {
 	}
 }
 
-async function getSelectionResults(postData){
+async function getSelectionResults(postData) {
 	let getSelectionResultsSQL = `
-	SELECT u.*, ut.type,
-	cs1.site_name AS choice_1_site_name,
-	cs2.site_name AS choice_2_site_name,
-	cs3.site_name AS choice_3_site_name,
-	cs4.site_name AS choice_4_site_name,
-	cs5.site_name AS choice_5_site_name
-  	FROM users u
-  	JOIN user_type ut ON u.user_type_id = ut.user_type_id
-  	LEFT JOIN student_choices sc ON sc.user_id = u.user_id
-  	LEFT JOIN clinical_sites cs1 ON cs1.clinical_sites_id = sc.choice_1
-  	LEFT JOIN clinical_sites cs2 ON cs2.clinical_sites_id = sc.choice_2
-  	LEFT JOIN clinical_sites cs3 ON cs3.clinical_sites_id = sc.choice_3
-  	LEFT JOIN clinical_sites cs4 ON cs4.clinical_sites_id = sc.choice_4
-  	LEFT JOIN clinical_sites cs5 ON cs5.clinical_sites_id = sc.choice_5
-  	WHERE u.MRAD_id = :MRADid;
+	SELECT name, MRAD_id, '1' as Choice, c1p1.site_name as Site1, c1p2.site_name as Site2, c1p3.site_name as Site3
+	FROM student_choices
+	join users on users.user_id = student_choices.user_id
+	join line_options on student_choices.choice_1 = line_options.line_option_id
+	join clinical_sites c1p1 on c1p1.clinical_sites_id = line_options.placement_one
+	join clinical_sites c1p2 on c1p2.clinical_sites_id = line_options.placement_two
+	join clinical_sites c1p3 on c1p3.clinical_sites_id = line_options.placement_three
+	WHERE MRAD_id = :MRADid
+	UNION
+	SELECT name, MRAD_id, '2' as Choice, c2p1.site_name as Site1, c2p2.site_name as Site2, c2p3.site_name as Site3
+	FROM student_choices
+	join users on users.user_id = student_choices.user_id
+	join line_options on student_choices.choice_2 = line_options.line_option_id
+	join clinical_sites c2p1 on c2p1.clinical_sites_id = line_options.placement_one
+	join clinical_sites c2p2 on c2p2.clinical_sites_id = line_options.placement_two
+	join clinical_sites c2p3 on c2p3.clinical_sites_id = line_options.placement_three
+	WHERE MRAD_id = :MRADid
+	UNION
+	SELECT name, MRAD_id, '3' as Choice, c3p1.site_name as Site1, c3p2.site_name as Site2, c3p3.site_name as Site3
+	FROM student_choices
+	join users on users.user_id = student_choices.user_id
+	join line_options on student_choices.choice_3 = line_options.line_option_id
+	join clinical_sites c3p1 on c3p1.clinical_sites_id = line_options.placement_one
+	join clinical_sites c3p2 on c3p2.clinical_sites_id = line_options.placement_two
+	join clinical_sites c3p3 on c3p3.clinical_sites_id = line_options.placement_three
+	WHERE MRAD_id = :MRADid
+	UNION
+	SELECT name, MRAD_id, '4' as Choice, c4p1.site_name as Site1, c4p2.site_name as Site2, c4p3.site_name as Site3
+	FROM student_choices
+	join users on users.user_id = student_choices.user_id
+	join line_options on student_choices.choice_4 = line_options.line_option_id
+	join clinical_sites c4p1 on c4p1.clinical_sites_id = line_options.placement_one
+	join clinical_sites c4p2 on c4p2.clinical_sites_id = line_options.placement_two
+	join clinical_sites c4p3 on c4p3.clinical_sites_id = line_options.placement_three
+	WHERE MRAD_id = :MRADid
+	UNION
+	SELECT name, MRAD_id, '5' as Choice, c5p1.site_name as Site1, c5p2.site_name as Site2, c5p3.site_name as Site3
+	FROM student_choices
+	join users on users.user_id = student_choices.user_id
+	join line_options on student_choices.choice_5 = line_options.line_option_id
+	join clinical_sites c5p1 on c5p1.clinical_sites_id = line_options.placement_one
+	join clinical_sites c5p2 on c5p2.clinical_sites_id = line_options.placement_two
+	join clinical_sites c5p3 on c5p3.clinical_sites_id = line_options.placement_three
+	WHERE MRAD_id = :MRADid;
 	`;
 
-	
+	let params = {
+		MRADid: postData.MRADid,
+	};
 
-    let params = {
-        MRADid: postData.MRADid 
-    };
-
-    try {
-        const results = await database.query(getSelectionResultsSQL, params);
-        console.log("Successfully found student selection results.");
-        console.log(results);
-        return results[0];
-    } catch (err) {
-        console.log("Error trying to find student selection results.");
-        console.log(err);
-        return false;
-    }
+	try {
+		const results = await database.query(getSelectionResultsSQL, params);
+		console.log("Successfully found student selection results.");
+		console.log(results);
+		return results[0];
+	} catch (err) {
+		console.log("Error trying to find student selection results.");
+		console.log(err);
+		return false;
+	}
 }
-
 
 module.exports = {
 	getStudents,
 	getOneStudent,
-	getSelectionResults
+	getSelectionResults,
 };
